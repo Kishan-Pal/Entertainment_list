9332882a560b9c05c561d6f9f0667078c583386b
Kishan Pal <kishanpalh6145@gmail.com>
2024-05-10 16:43:01 +0530

    modified style of nav

--- src/Nav.js
+++ src/Nav.js
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav>
-      <ul class="flex flex-row">
-        <Link to="/anime" class="p-2">
+      <ul class="flex flex-row *:p-2 bg-[#1a2077] *:text-[#fff]">
+        <Link to="/anime" class="hover:bg-[#ff8383c4]">
           Anime
         </Link>
-        <Link to="/movies" class="p-2">
+        <Link to="/movies" class="hover:bg-[#ff8383c4]">
           Movies
         </Link>
-        <Link to="/web-series" class="p-2">
+        <Link to="/web-series" class="hover:bg-[#ff8383c4]">
           Web series
         </Link>
       </ul>
